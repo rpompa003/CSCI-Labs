656849650811d6b25a61257a642605c95ee40ef6
rpompa003 <rpompa003@student.butte.edu>
2024-10-29 00:44:19 +0000

    finish debugging

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,79 +1,43 @@
 //Made by Rafael Pompa
-//Created on 10/12
-//This program is to show a paramter and return in a fuction
-#include <iostream>
+//Created on 10/28
+//This program is to show Debugging (with loops)
+
+#include<iostream>
 #include <cmath>
 using namespace std;
 
-void Store_coworker_sales(){
-cout<<"Hey Bob, how many $600 Tvs have you sold so far? ";
-}
+//Setting up the coding Process
+int main() {
+  float a = 0.0;
+  float m = 0.0;
+  float r = 0.0;
+  float y = 0.0;  
 
-float calcuting_for_commission(float total_price){//function declaration with function name calcuting_for_commission
-  float commission;
-//if price is over 250,000
-  if (total_price>=250000){
-  commission=total_price*0.0675;
-}
-//if the price is under 50,000
-  else if (total_price<50000){
-  commission= total_price*0.04;
-}
-//if the price is between 50,000 and 250,000
-  else if (total_price >= 50000 && total_price <= 250000){
-  commission=total_price*0.06;
-}
-//returing commission price amount
-return commission;
-}
+// user input for amount borrowed
+  cout<<"Enter the amount borrowed: ";
+  cin>>a;
+// user input for monthly payment
+  cout<<"Enter the monthly payment you'll make: ";
+  cin>>m;
+// user input for intrest rate
+  cout<<"Enter the annual interest rate: ";
+  cin>>r;
+// user input for years simulated
+  cout<<"Enter the number of years you want to simulate: ";
+  cin>>y;
 
-float calcuting_for_commission_percent(float total_price){//function declaration with function name calcuting_for_commission_percent
-float percent;
-//calculating commission percent
-//if price is over 250,000
-if (total_price>=250000){
-percent=6.75;
-}
-//if the price is under 50,000
-else if (total_price<50000){
-percent=4;
-}
-//if the price is between 50,000 and 250,000
-else if (total_price >= 50000 && total_price <= 250000){
-percent=6;
-}
-//returing commission percentage
-return percent;
-}
+// convert interest rate from percent to fraction
+  r = r/100;
 
-int main(){
-  //SEtting up the coding Process
-  int Amount_sold=0;
-  float total_price=1;
-  float commission =0;
-  float percent =0;
-  float Tv_Price=600;
+//iterate through each monthly
+  for (int i = 0; i < 12 * y; i++){
 
-//starting sentence
-Store_coworker_sales();
-//userinput for how ,amy tvs were sold
-cin>>Amount_sold;
-cout<<" "<<endl;
-//finding total price of the tvs
-total_price= Amount_sold * Tv_Price;
+//calculate additional interest
+    float I = a*(r/12);
 
-commission = calcuting_for_commission( total_price );//function call
+//add interest and subtract payment from balance
+    a = a + I - m;
+  }
 
-percent = calcuting_for_commission_percent( total_price);//function call
-
-//final outputs for tvs sold, total price, commission made, and percent commission is at.
-cout<<"tvs sold: "<<Amount_sold<<endl;
-cout<<"total price: $"<<total_price<<endl;
-cout<<"comminssions made: $"<<commission<<endl;
-cout<<"percent that the commission is at "<<percent <<"%"<<endl;
-cout<<" "<<endl;
-
-//final sentence showing all outputs
-cout<<"You know that scene you sold "<<Amount_sold<<" tvs, you would have made $"<<total_price<<" dollars selling tvs." <<endl;
-cout<<"That means you would get $"<< commission <<" in comminssions at a "<<percent<<"%."<<endl;
+cout<<"At the end of "<<y<<" years you owe "<<a<<" dollars";
 }
